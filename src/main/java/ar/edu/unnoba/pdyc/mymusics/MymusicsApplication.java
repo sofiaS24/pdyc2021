1e5f424529bb562a10ed9f0c2eeb9ee3493e1566
Sofia Sanchez <sosanchez@cdainfo.local>
2021-05-08 09:43:49 -0300

    primer commit tp2 pdyc2021

---
+++ src/main/java/ar/edu/unnoba/pdyc/mymusics/MymusicsApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.pdyc.mymusics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymusicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymusicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unnoba/pdyc/mymusics/MymusicsApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.pdyc.mymusics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymusicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
