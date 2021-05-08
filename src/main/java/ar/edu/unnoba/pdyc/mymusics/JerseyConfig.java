4a9f2f620a0a24c43d9e7a58d0a210c039c4d049
Sofia Sanchez <sosanchez@cdainfo.local>
2021-05-08 11:24:32 -0300

    Se modifico el archivo properties y se agrego la clase jerseyConfig

---
+++ src/main/java/ar/edu/unnoba/pdyc/mymusics/JerseyConfig.java
@@ -0,0 +1,15 @@
+package ar.edu.unnoba.pdyc.mymusics;
+
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JerseyConfig extends ResourceConfig 
+{
+    public JerseyConfig() 
+    {
+        
+    }
+}
+
